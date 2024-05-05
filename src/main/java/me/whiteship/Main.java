66aceae885be25ef983542982dbf17e1ff331087
JinJungs <nehciyz@gmail.com>
2024-05-05 16:34:03 +0900

    클래스 로더

--- src/main/java/me/whiteship/Main.java
+++ src/main/java/me/whiteship/Main.java
@@ -2,6 +2,8 @@ package me.whiteship;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        ClassLoader classLoader = Main.class.getClassLoader();
+        System.out.println(classLoader);
+        System.out.println(classLoader.getParent());
     }
 }
